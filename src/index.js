871cdaa1e665b44592904b7fede504287da1fea2
Dhavamani07 <dhavamani774604@gmail.com>
2026-07-20 20:07:22 +0530

    Use HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./Dashboard";
 import Admin from "./Admin";
@@ -9,10 +9,10 @@ import Admin from "./Admin";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/admin" element={<Admin />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
